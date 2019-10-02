dc8eeb5df322286a284ae5e9fa586a1e877dbf4d
JoeLorenzo <Joselmedrano@gmail.com>
2019-10-02 17:51:09 -0500

    -Added a test data file as data.js -imported data to app.js and set the state data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import data from './data';
+
+
+
 
 class App extends React.Component {
+  constructor() {
+    super()
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+    this.state = {
+      tasks: data
+    }
+  }
+
   render() {
     return (
       <div>
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    task: 'Mop the kitchen',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Meal prep for the week',
+    id: 2,
+    completed: false
+  },
+  {
+    task: 'Buy groceries',
+    id: 3,
+    completed: false
+  },
+  {
+    task: 'Take out the trash',
+    id: 4,
+    completed: false
+  }
+];
